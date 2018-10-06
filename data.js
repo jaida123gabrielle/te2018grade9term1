5ad57e54ba414c7c247aa2ec0f4af48ffc3db3da
jaida <jaidao@csedge.org>
2018-10-06 13:19:01 -0700

    Updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,19 @@
 // Example
-let message = "Hello World!";
+let message = "Hello Jaida!";
 console.log(message);
 
 // Type your code below!
+ let BdayMonth = 12;
+ let FavoriteNumber = 2;
+ let MyGPA = 4.0;
+ console. log(Bday)
+
+ let message2 = "We the people";
+ let message3 = "We matter";
+ let message4 = "We manifest our own destiny";
+
+ let FightForYourRights = true;
+ let BeWhoYouAre = true;
+ let HonorTheStatusQuo = false;
+
+ let CollegeChoice = undefined;
