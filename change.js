fed46d10197c1b4bcfa666406a10922dc26ead4a
jaida <jaidao@csedge.org>
2018-10-13 13:02:18 -0700

    new ways to combine things
    learned new ways to combine things

--- change.js
+++ change.js
@@ -1,7 +1,46 @@
-// import readline-sync module, don't touch!
-const READLINE = require("readline-sync");
+const READLINE = require('readline-sync');
+//let userInput = READLINE.question("Type anything and I'll repeat it back!");
+//console.log(userInput);
 
-// fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
 
-// write code below that simulates the change machine in action!
+// declare and assign a variable for pennies using readlineSync
+// ask the user how many pennies he/she wants change for
+
+let numOfPennies = READLINE.question("Please cents as a positive integer");
+
+console.log(numOfPennies);
+
+
+
+// declare, assign, and print how many quarters you can make out of the pennies
+let quarters = Math.floor(numOfPennies/25)
+
+console.log(quarters,"QUARTERS");
+
+
+
+
+// declare, assign, and print how many dimes you can make out of the pennies
+
+
+
+
+
+// declare, assign, and print how many nickels you can make out of the pennies
+
+
+
+
+
+// declare, assign, and print how many pennies are left over
+
+
+
+
+
+// make a change drawer using an object to
+// store all of your change
+// each property on the object should represent the particular coin
+// use the dot operator on the object to assign how many of each coin you have
+// many coins are in the drawer
+// print the drawer to see that the values are correct
---
+++ objects.js
@@ -0,0 +1,24 @@
+let currentYear = {
+  number: 2018,
+  leapYear: false
+};
+let person = {
+  name: "Bri",
+  age: 14,
+  likesChicken: true
+};
+let favShow = {
+  title: "Dynasty",
+  onNetflix: true,
+  favCharacter: "Sammy Joe",
+  numOfSeasons: 1
+};
+let siblings = {
+  number: 5,
+  wantsToBeOnlyChild: false,
+  livesInSameHouse: false
+};
+console.log('The current year is ' +  currentYear.number + '.');
+console.log("My partner's name is " + person.name);
+console.log("Her favorite show is " + favShow.onNetflix);
+console.log("She has" +  siblings.number  + "siblings");
