7ae1a25078d1316972b565dc9f5a195e5d6b8602
jaida <jaidao@csedge.org>
2018-10-20 15:36:40 -0700

    Update conditionals.js
    I completed problem one of the assigement

--- conditionals.js
+++ conditionals.js
@@ -7,6 +7,12 @@ const READLINE = require("readline-sync");
 console.log("*** Problem 1: R-rated ***");
 let age = READLINE.question("Enter your age: ");
 let withAdult = READLINE.question("Are you with an adult? (yes or no): ");
+if (age >= 18) {
+  console.log("You cannot see this movie");
+} else if (withAdult == 'yes') {
+   console.log("You can watch the movie");
+
+}
 
 
 // **** Problem 2: Umbrella ****
@@ -14,27 +20,27 @@ let withAdult = READLINE.question("Are you with an adult? (yes or no): ");
 // However, if it is thunderstorming, you should not bring an umbrella since
 // that's bad luck. Write code that prints if someone should bring an umbrella
 // with them.
-console.log("*** Problem 2: Umbrella ***");
-let raining = READLINE.question("Is it raining? (yes or no): ");
-let thunderstorming = READLINE.question("Is it thunderstorming? (yes or no): ");
+//console.log("*** Problem 2: Umbrella ***");
+//let raining = READLINE.question("Is it raining? (yes or no): ");
+//let thunderstorming = READLINE.question("Is it thunderstorming? (yes or no): ");
 
 
 // **** Problem 3: Monkey Trouble ****
 // There are two monkeys: Bubbles and Spankey. You are in trouble if both of
 // them are smiling, or if neither are smiling. Write code that prints if we
 // are in trouble.
-console.log("*** Problem 3: Monkey Trouble ***");
-let bubbles = READLINE.question("Is Bubbles smiling? (yes or no): ");
-let spankey = READLINE.question("Is Spankey smiling? (yes or no): ");
+//console.log("*** Problem 3: Monkey Trouble ***");
+//let bubbles = READLINE.question("Is Bubbles smiling? (yes or no): ");
+//let spankey = READLINE.question("Is Spankey smiling? (yes or no): ");
 
 
 // **** Problem 4: First Place ****
 // Write code that prints the largest of three scores. If there is a tie for
 // first place, simply print one of the tie winners.
-console.log("*** Problem 4: First Place ***");
-let score1 = READLINE.question("Enter the first score: ");
-let score2 = READLINE.question("Enter the second score: ");
-let score3 = READLINE.question("Enter the third score: ");
+//console.log("*** Problem 4: First Place ***");
+//let score1 = READLINE.question("Enter the first score: ");
+//let score2 = READLINE.question("Enter the second score: ");
+//let score3 = READLINE.question("Enter the third score: ");
 
 
 // **** Problem 5: Phone Shopping ****
@@ -46,8 +52,8 @@ let score3 = READLINE.question("Enter the third score: ");
 //    If you have at least $600 you can buy a top-tier phone.
 // Write code that prints all the phone tiers someone can buy based on how much
 // money they have.
-console.log("*** Problem 5: Phone Shopping ***");
-let money = READLINE.question("How much money do you have?: ");
+//console.log("*** Problem 5: Phone Shopping ***");
+//let money = READLINE.question("How much money do you have?: ");
 
 
 // **** Problem 6: Guess My Number ****
@@ -56,6 +62,6 @@ let money = READLINE.question("How much money do you have?: ");
 // a congratulations message. If they guess too low or too high, tell them this.
 // If their guess is myNum +/- 3, tell them they are "warm"; otherwise tell
 // them they are cold.
-console.log("*** Problem 6: Guess My Number ***");
-let myNum = 5;
-let guess = READLINE.question("I'm thinking of a number. Guess it: ");
+//console.log("*** Problem 6: Guess My Number ***");
+//let myNum = 5;
+//let guess = READLINE.question("I'm thinking of a number. Guess it: ");
